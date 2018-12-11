7ccb99b7b0b5e43adaa90a66fdecb6087c28e66f
vicotr <victor@within.co.kr>
2018-12-11 23:54:32 +0900

    builder 패턴에서 막힘.

---
+++ src/main/java/com/victor/webservice/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.victor.webservice.domain.posts;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Posts
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
